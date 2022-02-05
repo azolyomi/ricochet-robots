043dfa82428a3e1e91cd2dc526e43d5c79649750
Andras Zolyomi <azolyomi@169-231-150-202.wireless.ucsb.edu>
2022-02-05 14:01:34 -0800

    robot moving yay

--- rrgame/src/classes/BoardObject.js
+++ rrgame/src/classes/BoardObject.js
@@ -1,17 +1,66 @@
 import Tile from './TileObject';
 
+const DIRECTIONS = ["left", "right", "up", "down"];
+
 export default class Board {
     constructor() {
         this.height = 16;
         this.width = 16;
         let t = [];
         for (let i=0; i<this.height; i++) {
+            t.push([]);
             for (let j=0; j<this.width; j++) {
-                t.push(new Tile())
+                let walls = {up: i === 0, right: j === this.width-1, down: i === this.height - 1, left: j === 0}
+                t[i].push(new Tile(walls, {x: i, y: j}, null, null, null))
             }
         }
         this.tiles = t;
+
+        this.tiles[4][4].setRobot('yellow');
     }
+
+
+    moveRobot(xpos, ypos, direction) {
+        if (!(this.tiles[xpos][ypos].robot)) return false;
+        if (!(DIRECTIONS.includes(direction))) return false;
+
+        console.log("trying to move...");
+
+        let i=xpos, j=ypos, inext = xpos, jnext = ypos;
+        let robot = this.tiles[i][j].robotOff();
+        while (this.tiles[i][j].canMove(direction)) {
+            if (direction === "up") inext--;
+            else if (direction === "down") inext++;
+            else if (direction === "right") jnext++;
+            else jnext--;
+
+            if (this.tiles[inext][jnext]?.canBeMovedTo(direction)) {
+                i = inext;
+                j = jnext;
+            }
+            else break;
+        }
+        this.tiles[i][j].robotOn(robot);
+        return this.tiles[i][j];
+    }
+
+    calcDir(from, to) {
+        let a = from.pos.x === to.pos.x;
+        let b = from.pos.y === to.pos.y;
+        if (a && b) return false;
+        if (a) {
+            if (from.pos.y > to.pos.y) return "left";
+            return "right"
+        }
+        else if (b) {
+            if (from.pos.x > to.pos.x) return "up";
+            return "down"
+        }
+        return false;
+        
+    }
+
+    
 
     get dimensions() {
         return (this.height, this.width);
--- rrgame/src/classes/TileObject.js
+++ rrgame/src/classes/TileObject.js
@@ -1,9 +1,59 @@
+const POSSIBLE_ROBOTS = ['yellow', 'blue', 'red', 'green', 'silver'];
+const ROBOT_WALLS = {
+    up: true,
+    right: true,
+    down: true,
+    left: true
+}
 export default class Tile {
-    constructor(walls=null, target=null, color=null) {
+    constructor 
+    (
+        walls, 
+        pos = {x: null, y: null},
+        robot = null,
+        target = null, 
+        color = null,
+    ) {
+        this.robot = this.parseRobot(robot);
         this.target = target;
-        let w = walls ?? [false, false, false, false];
-        this.walls = w;
+        this.cached_walls = walls;
+        this.walls = this.robot ? ROBOT_WALLS : walls;
         this.color = color;
+        this.pos = pos;
+    }
+
+    setRobot(robot) {
+        if (this.parseRobot(robot)) this.robot = robot;
+    }
+
+    parseRobot(robot) {
+        return (POSSIBLE_ROBOTS.includes(robot) ? robot : null);
+    }
+
+    canMove(dir) {
+        return !(this.walls[dir]);
+    }
+    canBeMovedTo(toDir) {
+        return !(this.walls[this.fromDir(toDir)] || this.robot);
+    }
+
+    fromDir(toDir) {
+        if (toDir === "up") return "down";
+        if (toDir === "down") return "up";
+        if (toDir === "right") return "left";
+        if (toDir === "left") return "right";
+        return false;
+    }
+
+    robotOff() {
+        this.walls = this.cached_walls;
+        let robot = this.robot;
+        this.robot = null;
+        return robot;
+    }
+    robotOn(robot) {
+        this.walls = ROBOT_WALLS;
+        this.robot = robot;
     }
 
     setColor(c) {
